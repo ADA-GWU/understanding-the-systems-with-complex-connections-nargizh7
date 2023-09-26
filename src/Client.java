392eb28a3d2feaf5c33a3df65f1d5e993758aab6
nargizh7 <nargizheybatova07@gmail.com>
2023-09-26 22:43:25 +0400

    client specifies the server - server outputs the result

--- src/Client.java
+++ src/Client.java
@@ -1,44 +1,47 @@
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
+        // check if the correct number of cli arguments is provided
         if (args.length != 3) {
             System.err.println("Usage: java Client <port1> <port2> <port3>");
             System.exit(1);
         }
 
+        // scanner to read input from the console
         Scanner consoleInput = new Scanner(System.in);
 
         try {
-            Socket[] serverSockets = new Socket[3]; // store connections to 3 server instances
-            ObjectOutputStream[] serverOutputs = new ObjectOutputStream[3];
-            ObjectInputStream[] serverInputs = new ObjectInputStream[3];
+            // arrays to hold sockets and output streams for three servers
+            Socket[] serverSockets = new Socket[3];
+            PrintWriter[] serverOutputs = new PrintWriter[3];
 
-            // Get server ports from command line arguments
-            int[] serverPorts = {Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])};
-
-            for (int i = 0; i < 3; i++) {
-                serverSockets[i] = new Socket("localhost", serverPorts[i]); // connect to the server instances
-                serverOutputs[i] = new ObjectOutputStream(serverSockets[i].getOutputStream());
-                serverInputs[i] = new ObjectInputStream(serverSockets[i].getInputStream());
-            }
+            // parse the server port numbers from cli arguments
+            int[] serverPorts = { Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]) };
 
             while (true) {
                 System.out.print("Enter a number: ");
+                // read integer input 
                 int userInput = Integer.parseInt(consoleInput.nextLine());
 
-                // Round-robin selection of servers
+                // determine which server will process the request 
                 int selectedServer = userInput % 3;
+                System.out.println("Request will be processed by server on port: " + serverPorts[selectedServer]);
 
-                serverOutputs[selectedServer].writeInt(userInput); // send the input to the selected server
+                // socket to connect to the selected server
+                serverSockets[selectedServer] = new Socket("localhost", serverPorts[selectedServer]);
+                // send data to the server
+                serverOutputs[selectedServer] = new PrintWriter(new OutputStreamWriter(serverSockets[selectedServer].getOutputStream()));
+
+                // send the user input to the selected server
+                serverOutputs[selectedServer].println(userInput);
                 serverOutputs[selectedServer].flush();
 
-                int serverResponse = serverInputs[selectedServer].readInt(); // receive the response
-                System.out.println("Server response: " + serverResponse);
+                // wait to allow the server to process the request
+                Thread.sleep(1000);
             }
 
         } catch (Exception e) {
@@ -46,6 +49,3 @@ public class Client {
         }
     }
 }
-
-
-
--- src/Server.java
+++ src/Server.java
@@ -1,24 +1,29 @@
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.io.*;
 
 public class Server {
     public static void main(String[] args) {
+    	// check if the correct number of cli arguments is provided
         if (args.length != 1) {
             System.err.println("Usage: java Server <port>");
             System.exit(1);
         }
 
+        // parse the server port number from the cli argument
         int port = Integer.parseInt(args[0]);
 
         try {
+            // server socket to listen for client connections
             ServerSocket serverSocket = new ServerSocket(port);
 
             while (true) {
+                // accept a client connection and communicate with the client
                 Socket client = serverSocket.accept();
+                // new instance of ServerHandler to handle the client's request
                 ServerHandler handler = new ServerHandler(client);
+                // new thread to handle the client's request concurrently
                 handler.start();
             }
         } catch (Exception e) {
@@ -28,34 +33,38 @@ public class Server {
 }
 
 class ServerHandler extends Thread {
+    // atomic counter to keep track of the number of client threads
     private static AtomicInteger counter = new AtomicInteger(0);
     private final Socket clientSocket;
 
     public ServerHandler(Socket clientSocket) {
+        // initialize the client socket
         this.clientSocket = clientSocket;
     }
 
     @Override
     public void run() {
         try {
-            ObjectOutputStream clientOutput = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream clientInput = new ObjectInputStream(clientSocket.getInputStream());
+            // BufferedReader reads data from the client
+            BufferedReader clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             while (true) {
-                int clientDataInput = clientInput.readInt(); // read data from the client
-                int result = clientDataInput * 2;
-                
-                // Print the result on the server terminal
-                System.out.println("Processed request: " + result);
-                
-                clientOutput.writeInt(result); // send back the result to the client
-                clientOutput.flush(); // flush the output stream to ensure data is sent immediately
+                // read a line of data from the client
+                String clientDataInput = clientInput.readLine();
+                // check if the client has disconnected
+                if (clientDataInput == null) {
+                    break;
+                }
+                // parse the received data as an integer
+                int userInput = Integer.parseInt(clientDataInput);
+                // multiply by 2 
+                int result = userInput * 2;
+
+                // print the result on the server's terminal
+                System.out.println("Processed request on port " + clientSocket.getLocalPort() + ": " + result);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
